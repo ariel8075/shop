0c71d72dd3f3fbceae851ea6d1e4ec3db0811a7f
ariel8075 <55179880+ariel8075@users.noreply.github.com>
2019-12-12 23:04:44 +0800

    Create AA.js

---
+++ AA.js
@@ -0,0 +1,29 @@
+
+
+/*側邊攔*/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "180px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+/*所有商品*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
