d74d2eeac77b7a322f4ab050706e18844b7f2de0
ariel8075 <55179880+ariel8075@users.noreply.github.com>
2019-12-14 03:20:05 +0800

    Update AA.js

--- AA.js
+++ AA.js
@@ -1,29 +1,52 @@
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+/*側邊欄下拉選單*/
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-
-/*側邊攔*/
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+/*側邊欄*/
 function openNav() {
-  document.getElementById("mySidenav").style.width = "180px";
+  document.getElementById("mySidenav").style.width = "190px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-/*所有商品*/
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+																		/*Banner圖片轉換 */
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
