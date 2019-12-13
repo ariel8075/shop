5182c942de5e16daf34ddb76048464e62099873f
rozan520126 <55341033+rozan520126@users.noreply.github.com>
2019-12-14 01:24:10 +0800

    導航欄終於成形

--- AA.js
+++ AA.js
@@ -1,29 +1,24 @@
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+/*側邊欄下拉選單*/
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-
-/*側邊攔*/
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+/*側邊欄*/
 function openNav() {
-  document.getElementById("mySidenav").style.width = "180px";
+  document.getElementById("mySidenav").style.width = "190px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-}
-/*所有商品*/
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
 }
