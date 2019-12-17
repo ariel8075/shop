e78e582ca047a19f9efd2337eb226b7cc584a9b2
ariel8075 <55179880+ariel8075@users.noreply.github.com>
2019-12-18 02:54:09 +0800

    Update information.js

--- information.js
+++ information.js
@@ -58,3 +58,8 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
